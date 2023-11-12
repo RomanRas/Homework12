cba9a915fab96d4b9adc2fd43f27389951c00734
RomanRas <kannidze@gmail.com>
2023-11-12 22:44:39 +1000

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.time.Year;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("Александр", "Блок");
+        Author author2 = new Author("Фёдор", "Достоевский");
+        Book book1 = new Book("Граф Монте-Кристо", author1, 1845);
+        Book book2 = new Book("Преступление и наказание", author2, 1866);
+        book1.setPublishingYear(Year.now().getValue());
+        System.out.println("book1.getPublishingYear() = " + book1.getPublishingYear());
     }
 }
